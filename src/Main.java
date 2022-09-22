4565932ac8e08d7cb87ba3c2e157b834c8d14ebb
tucker isaacs <isaacstb@mail.uc.edu>
2022-09-22 15:17:54 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args)
+    {
+    int age = 22;
+    if(age>= 21)
+
+        System.out.println("21 or older! you get a wristband ");
+    }
+
+
+
+}
